e5b0d5e976427d148159f961f4741e94902341f2
Lacy <imlacy.ig@gmail.com>
2025-09-06 13:45:48 +0200

    Verification for missing permisions

--- CustomLogo/WebController.cs
+++ CustomLogo/WebController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using MediaBrowser.Common.Configuration;
@@ -23,62 +24,72 @@ namespace CustomLogo
             var bannerDark = Request.Form.Files["BannerDark"];
             var bannerLight = Request.Form.Files["BannerLight"];
 
-            if (logo != null && logo.Length > 0) {
-                if (System.IO.File.Exists(_appPaths.WebPath + "assets/img/icon-transparent.png")) {
-                    var path = Path.Combine(_appPaths.WebPath, "assets/img/icon-transparent.png");
-                    using (var stream = new FileStream(path, FileMode.Create)) {
-                        await logo.CopyToAsync(stream);
+            try
+                {
+                    if (logo != null && logo.Length > 0) {
+                        if (System.IO.File.Exists(_appPaths.WebPath + "assets/img/icon-transparent.png")) {
+                            var path = Path.Combine(_appPaths.WebPath, "assets/img/icon-transparent.png");
+
+                            using (var stream = new FileStream(path, FileMode.Create)) {
+                                await logo.CopyToAsync(stream);
+                            }
+
+                            string[] files = Directory.GetFiles(_appPaths.WebPath, "icon-transparent*", SearchOption.AllDirectories);
+
+                            foreach (string file in files)
+                            {
+                                using (var stream = new FileStream(file, FileMode.Create))
+                                    {
+                                        await logo.CopyToAsync(stream);
+                                    }
+                            }
+                        }
+                    }
+
+                    if (bannerDark != null && bannerDark.Length > 0) {
+                        if (System.IO.File.Exists(_appPaths.WebPath + "assets/img/banner-dark.png")) {
+                            var path = Path.Combine(_appPaths.WebPath, "assets/img/banner-dark.png");
+                            using (var stream = new FileStream(path, FileMode.Create)) {
+                                await logo.CopyToAsync(stream);
+                            }
+                        }
+
+                        string[] files = Directory.GetFiles(_appPaths.WebPath, "banner-dark*", SearchOption.AllDirectories);
+
+                        foreach (string file in files)
+                        {
+                            using (var stream = new FileStream(file, FileMode.Create))
+                                {
+                                    await bannerDark.CopyToAsync(stream);
+                                }
+                        }
+                    }
+
+                    if (bannerLight != null && bannerLight.Length > 0) {
+                        if (System.IO.File.Exists(_appPaths.WebPath + "assets/img/banner-light.png")) {
+                            var path = Path.Combine(_appPaths.WebPath, "assets/img/banner-light.png");
+                            using (var stream = new FileStream(path, FileMode.Create)) {
+                                await logo.CopyToAsync(stream);
+                            }
+                        }
+
+                        string[] files = Directory.GetFiles(_appPaths.WebPath, "banner-light*", SearchOption.AllDirectories);
+
+                        foreach (string file in files)
+                        {
+                            using (var stream = new FileStream(file, FileMode.Create))
+                                {
+                                    await bannerLight.CopyToAsync(stream);
+                                }
+                        }
                     }
                 }
-
-                string[] files = Directory.GetFiles(_appPaths.WebPath, "icon-transparent*", SearchOption.AllDirectories);
-
-                foreach (string file in files)
+            catch (UnauthorizedAccessException)
                 {
-                    using (var stream = new FileStream(file, FileMode.Create))
-                        {
-                            await logo.CopyToAsync(stream);
-                        }
+                    return Content(
+                        "<html><head></head><body>Jellyfin does not have write access. Please check the permissions and ensure the application has sufficient rights.<br><a href='/web/#/dashboard/plugins'>Retun to Jellyfin</a></body></html>",
+                        "text/html");
                 }
-            }
-
-            if (bannerDark != null && bannerDark.Length > 0) {
-                if (System.IO.File.Exists(_appPaths.WebPath + "assets/img/banner-dark.png")) {
-                    var path = Path.Combine(_appPaths.WebPath, "assets/img/banner-dark.png");
-                    using (var stream = new FileStream(path, FileMode.Create)) {
-                        await logo.CopyToAsync(stream);
-                    }
-                }
-
-                string[] files = Directory.GetFiles(_appPaths.WebPath, "banner-dark*", SearchOption.AllDirectories);
-
-                foreach (string file in files)
-                {
-                    using (var stream = new FileStream(file, FileMode.Create))
-                        {
-                            await bannerDark.CopyToAsync(stream);
-                        }
-                }
-            }
-
-            if (bannerLight != null && bannerLight.Length > 0) {
-                if (System.IO.File.Exists(_appPaths.WebPath + "assets/img/banner-light.png")) {
-                    var path = Path.Combine(_appPaths.WebPath, "assets/img/banner-light.png");
-                    using (var stream = new FileStream(path, FileMode.Create)) {
-                        await logo.CopyToAsync(stream);
-                    }
-                }
-
-                string[] files = Directory.GetFiles(_appPaths.WebPath, "banner-light*", SearchOption.AllDirectories);
-
-                foreach (string file in files)
-                {
-                    using (var stream = new FileStream(file, FileMode.Create))
-                        {
-                            await bannerLight.CopyToAsync(stream);
-                        }
-                }
-            }
 
             return Content(
                 "<html><head><meta http-equiv='refresh' content='0;url=/web/#/dashboard/plugins' /></head><body>Redirection...</body></html>",
